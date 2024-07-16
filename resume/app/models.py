f99db96ab281ecf0447c5972543ec250a8ced8c7
harihara9999 <patakanuruhariharanaidu@gmail.com>
2024-07-16 20:12:56 +0530

    first commit

---
+++ resume/app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class profile(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    about=models.TextField()
+    college=models.CharField(max_length=20)
+    degree =models.CharField(max_length=20)
+    project = models.TextField()
+class resumeprofile(models.Model):
+    resume_id = models.ForeignKey(profile, on_delete=models.CASCADE)
